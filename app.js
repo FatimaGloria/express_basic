1afcd17fed73c3cfb6ad04143c8c0de045f9528f
FatimaGloria <janettgloria1@gmail.com>
2022-05-23 22:13:16 -0500

    2. Agregando una nueva ruta al servidor

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.get('/',(req, res) => {
     res.send('Hello World!')
 })
 
-//Con esto inicializamos esta app
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+//Respondiendo texto
+//localhost:3000/launchx
+app.get('/launchx',(req, res) => {
+    res.send('Bienvenidos a launchx')
 })
