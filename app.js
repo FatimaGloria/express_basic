4a29c25f7860140cdb4c0f7ca5f2a5cd32c11888
FatimaGloria <janettgloria1@gmail.com>
2022-05-24 00:36:10 -0500

    2.1 Modificando cÃodigo para responder en /launchx

--- app.js
+++ app.js
@@ -15,3 +15,9 @@ app.get('/',(req, res) => {
 app.get('/launchx',(req, res) => {
     res.send('Bienvenidos a launchx')
 })
+
+//Con esto inicializamos esta app
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
